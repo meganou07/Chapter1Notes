d51aac0738002abb9d303f5bb02b547ba9c46fc9
7ezr <7ezr@fresnou.org>
2023-02-06 13:43:56 -0800

    Chapter 1 notes

---
+++ dataTypes.js
@@ -0,0 +1,35 @@
+console.log(3);
+//This is a comment
+
+//Numbers
+//Integers
+console.log(-3);
+console.log(0);
+console.log(7);
+
+//Floating Point Numbers (Floats)
+console.log(3.14);
+console.log(-0.123456);
+
+//Strings
+//Character, letters, symbols, spaces
+console.log("Hello World");
+console.log('!@#$ is also a string');
+console.log(`Backticks by the 1`);
+
+//Arithmetic Operators (Math)
+console.log(14+2);
+console.log(14-2);
+console.log(14*2); //shift-8 gives*
+console.log(14/2);
+console.log(14%2);//% is called MOD
+                 //it means remainder after dividing
+
+//Concatenation means to combine strings
+console.log("My name"+"is spiderman");
+console.log('My'+' '+'age'+' '+'is '+15+'.');
+console.log("Your age is\n16");
+
+//Template literal - plug in some calculated value
+// Use backticks `` by the 1 on your keyboard
+console.log(`15 divided by 10 = ${15/10} disohfpeo`);
